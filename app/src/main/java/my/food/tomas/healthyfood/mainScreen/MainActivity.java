bb104f10711a9868721084f23147e7728c402086
Tomas <tomasmaks@gmail.com>
2016-10-27 11:09:23 +0300

    MainFragment and MainActivity is going to be implemented

--- app/src/main/java/my/food/tomas/healthyfood/mainScreen/MainActivity.java
+++ app/src/main/java/my/food/tomas/healthyfood/mainScreen/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -24,8 +25,13 @@ import my.food.tomas.healthyfood.data.remote.AppRemoteDataStore;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnMainFragmentListener {
 
+    public static final String TAG = "MainActivity";
+
+    private MainFragment mainFragment;
     private SearchView searchView;
     private MenuItem searchClearItem;
+
+    private String prevSearchQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +47,6 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnMa
         startActivity(intent);
     }
 
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -52,6 +55,113 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnMa
         setupSearchView();
         searchClearItem = menu.findItem(R.id.action_search_clear);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_sort_top:
+                actionSortTop();
+                item.setChecked(true);
+                return true;
+            case R.id.action_sort_tranding:
+                actionSortTranding();
+                item.setChecked(true);
+                return true;
+            case R.id.action_view_list:
+                actionViewList();
+                item.setChecked(true);
+                return true;
+            case R.id.action_view_table:
+                actionViewTable();
+                item.setChecked(true);
+                return true;
+            case R.id.action_search_clear:
+                searchRecipe("");
+                searchClearItem.setVisible(false);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initFragment(Bundle savedInstanceState) {
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                mainFragment = (MainFragment)getSupportFragmentManager().findFragmentByTag(MainFragment.TAG);
+            }
+            createFragment();
+        }
+    }
+
+    private void createFragment() {
+        mainFragment = MainFragment.newInstance();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, mainFragment, MainFragment.TAG)
+                .commit();
+    }
+
+
+    private void setupSearchView() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchRecipe(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    searchView.setQuery(prevSearchQuery, false);
+                    searchClearItem.setVisible(false);
+                } else if (prevSearchQuery != null && prevSearchQuery.length() > 0) {
+                    searchClearItem.setVisible(true);
+                }
+            }
+        });
+    }
+
+    private void searchRecipe(String query) {
+        prevSearchQuery = query;
+        if (mainFragment != null) {
+            mainFragment.setSearchQuery(query);
+            mainFragment.startSearch();
+        }
+    }
+
+    private void actionSortTop() {
+        if (mainFragment != null) {
+            mainFragment.setSearchSortType("r");
+            mainFragment.startSearch();
+        }
+    }
+
+    private void actionSortTranding() {
+        if (mainFragment != null) {
+            mainFragment.setSearchSortType("t");
+            mainFragment.startSearch();
+        }
+    }
+
+    private void actionViewList() {
+        if (mainFragment != null) {
+            mainFragment.setViewList();
+            mainFragment.startSearch();
+        }
+    }
+
+    private void actionViewTable() {
+        if (mainFragment != null) {
+            mainFragment.setViewTable();
+            mainFragment.startSearch();
+        }
     }
 
 
--- app/src/main/java/my/food/tomas/healthyfood/mainScreen/MainFragment.java
+++ app/src/main/java/my/food/tomas/healthyfood/mainScreen/MainFragment.java
@@ -1,12 +1,30 @@
 package my.food.tomas.healthyfood.mainScreen;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
+import my.food.tomas.healthyfood.data.local.models.RecipeSearchParams;
 
 /**
  * Created by Tomas on 27/10/2016.
  */
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment  {
+
+    public static final String TAG = "MainFragment";
+
+    private RecipeSearchParams recipeSearchParams;
+
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        fragment.recipeSearchParams = new RecipeSearchParams();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MainFragment() {
+    }
 
 
 
