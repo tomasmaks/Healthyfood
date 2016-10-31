4f92e80c3952e9112019877fdd1af25c418cab2a
Tomas <tomasmaks@gmail.com>
2016-10-31 23:28:56 +0200

    XML file for recipe fragment was created

--- app/src/main/java/my/food/tomas/healthyfood/detailsScreen/RecipeFragment.java
+++ app/src/main/java/my/food/tomas/healthyfood/detailsScreen/RecipeFragment.java
@@ -2,6 +2,13 @@ package my.food.tomas.healthyfood.detailsScreen;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import my.food.tomas.healthyfood.R;
+import my.food.tomas.healthyfood.data.local.models.Recipe;
 
 
 /**
@@ -12,6 +19,21 @@ public class RecipeFragment extends Fragment {
 
     public static final String TAG = "RecipeFragment";
 
+    private View rootView;
+    @Bind(R.id.recipe_title_view)
+    TextView titleView;
+    @Bind(R.id.recipe_image_view)
+    ImageView imageView;
+    @Bind(R.id.recipe_publisher_view)
+    TextView publisherView;
+    @Bind(R.id.recipe_rank_view)
+    TextView rankView;
+    @Bind(R.id.recipe_ingredients_view)
+    TextView ingredientsView;
+
+    private String recipeId;
+    private Recipe recipe;
+
     public static RecipeFragment newInstance(String id) {
         RecipeFragment fragment = new RecipeFragment();
         Bundle args = new Bundle();
@@ -19,4 +41,10 @@ public class RecipeFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+    public RecipeFragment() {
+
+    }
+
+
 }
